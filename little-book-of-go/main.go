13cba57aa3d0f9fd22b9cb12767a7fba7f9a324b
paul pereyda karayan <paulkarayan@gmail.com>
2026-02-21 08:17:01 -0800

    mutex

--- little-book-of-go/main.go
+++ little-book-of-go/main.go
@@ -2,27 +2,25 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sort"
+	"sync"
+	"time"
+)
+
+var (
+	counter = 0
+	lock    sync.Mutex
 )
 
 func main() {
-	scores := make([]int, 100)
-	for i := 0; i < 100; i++ {
-		scores[i] = int(rand.Intn(1000))
+	for i := 0; i < 20; i++ {
+		go incr()
 	}
-	sort.Ints(scores)
-
-	worst := make([]int, 5)
-	// copy(worst, scores[:5])
-	copy(worst, scores[:7])
-
-	fmt.Println(worst)
-
+	time.Sleep(time.Millisecond * 10)
 }
 
-func removeAtIndex(source []int, index int) []int {
-	lastIndex := len(source) - 1
-	source[index], source[lastIndex] = source[lastIndex], source[index]
-	return source[:lastIndex]
+func incr() {
+	lock.Lock()
+	defer lock.Unlock()
+	counter++
+	fmt.Println(counter)
 }
