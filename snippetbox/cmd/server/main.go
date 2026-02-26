2c44c6afe7a9ebaa5aeec6fd843de09fd5d2f36f
paul pereyda karayan <paulkarayan@gmail.com>
2026-02-26 13:45:03 -0800

    move to TLS 1.2

--- snippetbox/cmd/server/main.go
+++ snippetbox/cmd/server/main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"crypto/tls"
+	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	mux := http.NewServeMux()
+	// TLS 1.2 vs. default of 1.0
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	srv := &http.Server{
+		Addr:         *addr,
+		Handler:      mux,
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	logger.Info("starting server on :4000")
+	logger.Info("starting server on", "addr", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error("handle error", "err", err)
 }
